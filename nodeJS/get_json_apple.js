e0f827db43dd286719fc8a7aaac71515619de158
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-04-07 23:57:02 -0500

    refactor(scraping): Unificar función de scraping para extraer datos de productos.

--- nodeJS/get_json_apple.js
+++ nodeJS/get_json_apple.js
@@ -1,48 +1,81 @@
-const puppeteer = require('puppeteer');
-const UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0';
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const UserAgent =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0";
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        executablePath: '/usr/bin/chromium-browser',
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
+async function scrapData() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
 
-    const page = await browser.newPage();
-    await page.setUserAgent(UserAgent);
-    await page.goto('https://www.apple.com/shop/refurbished/mac');
-    await page.waitForTimeout(5000);
+  const page = await browser.newPage();
+  await page.setUserAgent(UserAgent);
 
-    const data = await page.evaluate(() => JSON.stringify(window.REFURB_GRID_BOOTSTRAP));
+  await page.goto("https://www.apple.com/shop/refurbished/mac");
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-    await browser.close();
+  const macData = await page.evaluate(() =>
+    JSON.stringify(window.REFURB_GRID_BOOTSTRAP)
+  );
+  fs.writeFile(
+    "C:/xampp/htdocs/macbuho/json/mac.json",
+    macData,
+    function (err) {
+      if (err) return console.log(err);
+      console.log("Datos de Mac extraídos correctamente.");
+    }
+  );
 
-    fs = require('fs');
-    fs.writeFile('/www/macbuho.com/json/mac.json', data, function (err) {
-    if (err) return console.log(err);
-    });
+  await page.goto("https://www.apple.com/shop/refurbished/ipad");
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-})();
+  const ipadData = await page.evaluate(() =>
+    JSON.stringify(window.REFURB_GRID_BOOTSTRAP)
+  );
+  fs.writeFile(
+    "C:/xampp/htdocs/macbuho/json/ipad.json",
+    ipadData,
+    function (err) {
+      if (err) return console.log(err);
+      console.log("Datos de iPad extraídos correctamente.");
+    }
+  );
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        executablePath: '/usr/bin/chromium-browser',
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
+  await page.goto("https://www.apple.com/shop/refurbished/iphone");
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-    const page = await browser.newPage();
-    await page.setUserAgent(UserAgent);
-    await page.goto('https://www.apple.com/shop/refurbished/ipad');
-    await page.waitForTimeout(5000);
+  const iphoneData = await page.evaluate(() =>
+    JSON.stringify(window.REFURB_GRID_BOOTSTRAP)
+  );
+  fs.writeFile(
+    "C:/xampp/htdocs/macbuho/json/iphone.json",
+    iphoneData,
+    function (err) {
+      if (err) return console.log(err);
+      console.log("Datos de iPhone extraídos correctamente.");
+    }
+  );
 
-    const data = await page.evaluate(() => JSON.stringify(window.REFURB_GRID_BOOTSTRAP));
+  await page.goto("https:www.apple.com/shop/refurbished/watch");
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-    await browser.close();
+  const watchData = await page.evaluate(() =>
+    JSON.stringify(window.REFURB_GRID_BOOTSTRAP)
+  );
+  fs.writeFile(
+    "C:/xampp/htdocs/macbuho/json/watch.json",
+    watchData,
+    function (err) {
+      if (err) return console.log(err);
+      console.log("Datos de Watch extraídos correctamente.");
+    }
+  );
 
-    fs = require('fs');
-    fs.writeFile('/www/macbuho.com/json/ipad.json', data, function (err) {
-    if (err) return console.log(err);
-    });
+  
+  await browser.close();
+}
 
-})();
+scrapData();
+// setInterval(scrapData, 15 * 60 * 1000); // 15 minutos
+// setInterval(scrapData, 3 * 60 * 60 * 1000);
