74088dc7425c9cc00ba97978608d0c867315d8fc
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-04-07 23:55:53 -0500

    feat(touch): Agregar interacción para mostrar especificaciones al tocar en tarjeta de producto.

--- js/mobile.js
+++ js/mobile.js
@@ -1,12 +1,22 @@
-document.addEventListener('dblclick', function (e) {
-    e.preventDefault();
+document.addEventListener("dblclick", function (e) {
+  e.preventDefault();
 });
 
-document.addEventListener("touchstart", function(e){
-    e.preventDefault();
-});
+document.addEventListener(
+  "touchstart",
+  function (e) {
+    if (e.target.closest(".product-card")) {
+      e.preventDefault();
+      e.target.closest(".product-card").classList.toggle("show-specs");
+    }
+  },
+  { passive: false }
+);
 
-
-document.body.addEventListener('touchmove', function(event) {
-  event.preventDefault();
-}, false);
+document.body.addEventListener(
+  "touchmove",
+  function (event) {
+    event.preventDefault();
+  },
+  false
+);
