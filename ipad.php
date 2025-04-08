0e8e6c428ec1c2ace304139503d5c173d54f49e1
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-04-07 23:52:03 -0500

    refactor(productos): Mejorar validación de datos y valores por defecto

--- ipad.php
+++ ipad.php
@@ -4,45 +4,48 @@ include('section_head.php');
 
 // $data = json_decode(file_get_contents("./json_test/ipad.json"), true);
 $data = json_decode(file_get_contents("./json/ipad.json"), true);
-$total = count($data["tiles"]);
+
+$productos = $data["tiles"] ?? [];
+$total = count($productos);
 
 ?>
 
-    <div class="container" style="margin-top: 60px;">
-        <h1>Compra tu mac ahora<br> y recibe en 9 dias</h1>
-    </div>
-    
+<div class="container" style="margin-top: 60px;">
+    <h1>Compra tu iPad ahora<br> y recibe en 9 días</h1>
+</div>
+
 <div class="container">
     <div class="product_grid">
 
     <?php
-        for ($i=0; $i < $total; $i++) { 
+    for ($i = 0; $i < $total; $i++) {
+        $producto = $productos[$i];
 
-            $titulo = $data['tiles'][$i]['title'];
-            $precioString = (float)$data["tiles"][$i]["price"]["currentPrice"]["raw_amount"]*5.4-0.6;
-            $precio = number_format($precioString, 2);
-            $image = $data["tiles"][$i]["image"]["srcSet"]["src"];
+        $titulo = $producto['title'] ?? 'Sin título';
 
-            $disco = $data["tiles"][$i]["filters"]["dimensions"]["dimensionCapacity"];
-            $anio = $data["tiles"][$i]["filters"]["dimensions"]["dimensionRelYear"];
-            $ram = $data["tiles"][$i]["filters"]["dimensions"]["tsMemorySize"];
+        $precioRaw = $producto["price"]["currentPrice"]["raw_amount"] ?? null;
+        $precioString = $precioRaw ? ($precioRaw * 5.4 - 0.6) : 0;
+        $precio = number_format($precioString, 2);
 
-            $modelo = $data["tiles"][$i]["filters"]["dimensions"]["refurbClearModel"];
+        $image = $producto["image"]["srcSet"]["src"] ?? "ruta/por_defecto.jpg";
 
+        $dimensiones = $producto["filters"]["dimensions"] ?? [];
 
-            echo "<div class='product $modelo'>";
-            echo "<img src='$image'><br><br>";
-            echo "$titulo<br>";
-            echo "<span>$ram</span> <span>$disco</span> <span>$anio</span> <br><br>";
-            echo "S/$precio</div>";
-        }
+        $disco = $dimensiones["dimensionCapacity"] ?? "N/A";
+        $anio = $dimensiones["dimensionRelYear"] ?? "N/A";
+        $ram = $dimensiones["tsMemorySize"] ?? "N/A";
+
+        $modelo = $dimensiones["refurbClearModel"] ?? "modelo_desconocido";
+
+        echo "<div class='product $modelo'>";
+        echo "<img src='$image' alt='Imagen del producto'><br><br>";
+        echo htmlspecialchars($titulo) . "<br>";
+        echo "<span>$ram</span> <span>$disco</span> <span>$anio</span> <br><br>";
+        echo "S/$precio</div>";
+    }
     ?>
 
     </div>
 </div>
 
-<?php
-
-include('section_footer.php');
-
-?>
+<?php include('section_footer.php'); ?>
